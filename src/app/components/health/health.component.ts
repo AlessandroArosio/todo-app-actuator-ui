57fe1f1d3521bc9451b807d8be030c8c800b371c
Alessandro Arosio <alessandro.arosio1@gmail.com>
2020-02-15 16:41:09 +0000

    health page done

--- src/app/components/health/health.component.ts
+++ src/app/components/health/health.component.ts
@@ -9,6 +9,24 @@ import {Health} from '../../common/health/health';
 })
 export class HealthComponent implements OnInit {
   private health: Health;
+  private maxMemory = ' GB';
+  private currentMemory: string;
+
+  public allDataFetched = false;
+  public canvasWidth = 300;
+  public needleValue = 65;
+  public centralLabel = '';
+  public name = 'Disk space in use';
+  public bottomLabel = 'Calculating...';
+
+  public options = {
+    hasNeedle: true,
+    needleColor: 'black',
+    needleStartValue: 50,
+    arcColors: ['rgb(61,204,91)', 'rgb(239,214,19)', 'rgb(255,84,84)'],
+    arcDelimiters: [50, 75],
+    rangeLabel: ['0 GB', this.maxMemory],
+  };
 
   constructor(private actuatorService: ActuatorService) { }
 
@@ -18,9 +36,20 @@ export class HealthComponent implements OnInit {
 
   getHealth() {
     this.actuatorService.getHealth().subscribe(
-      data => this.health = data,
+      data => {
+        this.health = data;
+        this.updateGauge();
+        },
       error => console.log(error)
     );
+  }
+
+  private updateGauge() {
+    this.maxMemory = (this.health.components.diskSpace.details.total / 8589934592).toFixed(2) + ' GB';
+    this.options.rangeLabel[1] = this.maxMemory;
+    this.currentMemory = (this.health.components.diskSpace.details.free / 8589934592).toFixed(2);
+    this.bottomLabel = this.currentMemory;
+    this.allDataFetched = true;
   }
 
 }
